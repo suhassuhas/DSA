355e009b1e6f9d5b7efd4237661fa4d1bea15aec
suhassuhas <suhas7thfeb2000@gmail.com>
2022-05-12 22:40:51 +0530

    small changes

--- Leetcode/May-Day-12-2022-Permutations-ll.py
+++ Leetcode/May-Day-12-2022-Permutations-ll.py
@@ -1,38 +1,31 @@
-47. Permutations II
-Medium
+# 47. Permutations II
+# Medium
 
-5454
+# Add to List
 
-96
-
-Add to List
-
-Share
-Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+# Share
+# Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
 
  
 
-Example 1:
+# Example 1:
 
-Input: nums = [1,1,2]
-Output:
-[[1,1,2],
- [1,2,1],
- [2,1,1]]
-Example 2:
+# Input: nums = [1,1,2]
+# Output:
+# [[1,1,2],
+#  [1,2,1],
+#  [2,1,1]]
+# Example 2:
 
-Input: nums = [1,2,3]
-Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  
 
-Constraints:
+# Constraints:
 
-1 <= nums.length <= 8
--10 <= nums[i] <= 10
-Accepted
-635,746
-Submissions
-1,154,419
+# 1 <= nums.length <= 8
+# -10 <= nums[i] <= 10
+
 
 
 class Solution:
